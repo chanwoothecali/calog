51a661739728ae903463d3ed4e5b776e1e1ae0c1
chanwoothecali <jungcali94@gmail.com>
2024-04-01 17:31:35 +0900

    first commit

---
+++ calog/src/main/java/com/calog/CalogApplication.java
@@ -0,0 +1,13 @@
+package com.calog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalogApplication.class, args);
+	}
+
+}
---
+++ calog/src/test/java/com/calog/CalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
